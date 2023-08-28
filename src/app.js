797a2225ba7283f1de1ac91b9eadd52cab90b3d9
RebellionGX <agus210591@gmail.com>
2023-08-28 11:09:07 -0300

    mensaje

--- src/app.js
+++ src/app.js
@@ -1,22 +1,20 @@
-const path = require('path')
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static('public'));
-
-// app.listen(3030, () => console.log('Servidor abierto'));
-
-const port = process.env.PORT || 3030;
-app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+app.listen(3015,() => {
+    console.log('servidor corriendo en puerto 3015');
 });
 
-app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+app.get("/", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
 });
 
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
 });
+app.use('/static', express.static(__dirname + '/public'));
