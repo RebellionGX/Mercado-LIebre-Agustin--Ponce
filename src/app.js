e93e2ae415c0040b3a4a4f6da81fd6be2c046831
RebellionGX <agus210591@gmail.com>
2023-08-28 14:53:56 -0300

    mensaje

--- src/app.js
+++ src/app.js
@@ -8,13 +8,13 @@ app.listen(3015,() => {
 });
 
 app.get("/", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+    res.sendFile(path.resolve(__dirname, "./src/views/home.html"))
 })
 app.get("/register", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+    res.sendFile(path.resolve(__dirname, "./src/views/register.html"));
 });
 
 app.get("/login", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+    res.sendFile(path.resolve(__dirname, "./src/views/login.html"));
 });
 app.use('/static', express.static(__dirname + '/public'));
